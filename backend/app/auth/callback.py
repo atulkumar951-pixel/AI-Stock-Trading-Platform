ccd8be594d38c79b2527c714bff9608b7a44abda
Atul Kumar Mishra <matulkumar951@gmail.com>
2026-07-10 11:11:06 +0530

    .env value

--- backend/app/auth/callback.py
+++ backend/app/auth/callback.py
@@ -1,14 +1,33 @@
 from fastapi import APIRouter
+import requests
 
-router = APIRouter(
-    prefix="/auth",
-    tags=["Authentication"]
-)
+from app.core.config import settings
+
+router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 
 @router.get("/callback")
 def callback(code: str):
 
+    response = requests.post(
+        "https://api.upstox.com/v2/login/authorization/token",
+        headers={
+            "accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data={
+            "code": code,
+            "client_id": settings.UPSTOX_API_KEY,
+            "client_secret": settings.UPSTOX_API_SECRET,
+            "redirect_uri": settings.UPSTOX_REDIRECT_URI,
+            "grant_type": "authorization_code",
+        },
+    )
+
+    response.raise_for_status()
+
+    token = response.json()["access_token"]
+
     return {
-        "authorization_code": code
+        "access_token": token
     }
